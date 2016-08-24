ba544281e0e40134f95ff046bd4388c0b513cc9b
Ruth Bochere <rorbochere@gmail.com>
2016-08-24 03:57:54 +0300

    (Feature) Adds Usage and Options.
    Adds the Usage section, which describes the available commands and
    options, where as the Options section gives a description of what the
    options do. #1

---
+++ note_it.py
@@ -0,0 +1,19 @@
+from docopt import docopt
+
+'''This program is a Note taking application, known as 'Note It'.
+	The available commands are specified under Usage below.
+
+	Usage: 
+		note_it create <note_content> 
+		note_it view <note_id>
+		note_it delete <note_id>
+		note_it list (--limit)
+		note_it search <query_string> (--limit)
+		
+	Options:
+		-h, --help  shows this help message and exits
+		--limit		sets the number of items to display in resulting list  
+'''
+
+ 
+
